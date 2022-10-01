9b3593ae49aad4048780ea9a34dbf03b128c73d9
aquicor <aquinoacordero@gmail.com>
2022-10-01 21:56:42 +0200

    v1

---
+++ src/data.js
@@ -0,0 +1,43 @@
+export default [{
+        id: 1,
+        name: "Bertie Yates",
+        age: 29,
+        image: "https://cdn-icons-png.flaticon.com/512/147/147133.png"
+    },
+    {
+        id: 2,
+        name: "Hester Hogan",
+        age: 39,
+        image: "https://cdn-icons-png.flaticon.com/512/147/147133.png"
+    },
+    {
+        id: 3,
+        name: "Larry Little",
+        age: 19,
+        image: "https://cdn-icons-png.flaticon.com/512/147/147133.png"
+    },
+    {
+        id: 4,
+        name: "Yates Hogan",
+        age: 49,
+        image: "https://cdn-icons-png.flaticon.com/512/147/147133.png"
+    },
+    {
+        id: 5,
+        name: "Bertie Little",
+        age: 29,
+        image: "https://cdn-icons-png.flaticon.com/512/147/147133.png"
+    },
+    {
+        id: 6,
+        name: "Hester Yates",
+        age: 39,
+        image: "https://cdn-icons-png.flaticon.com/512/147/147133.png"
+    },
+    {
+        id: 7,
+        name: "Larry Hogan",
+        age: 29,
+        image: "https://cdn-icons-png.flaticon.com/512/147/147133.png"
+    },
+]
